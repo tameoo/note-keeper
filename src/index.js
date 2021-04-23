24edcae50294862ddaca9364d29ee57fd66ce1d1
tameooo <remat01@mail.ru>
2021-04-23 16:32:18 +0600

    extend database period

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
-import store from './store';
 import ErrorBoundary from './components/error-boundary';
-import {NoteServiceProvider} from './components/note-service-context';
-import NoteService from './services/';
-
-const noteService = new NoteService();
 
 ReactDOM.render(
   
-  <Provider store={store}>
     <ErrorBoundary>
-      <NoteServiceProvider value={noteService}>
         <Router>
           <App/>
         </Router>
-      </NoteServiceProvider>
     </ErrorBoundary>
-  </Provider>  
 
 , document.getElementById('root'));
--- src/services/note-service.js
+++ src/services/note-service.js
@@ -1,30 +1,33 @@
 export default class NoteService {
 
-    setfetchData =  async (card) => {
-        await fetch('https://note-keeper-e56d5-default-rtdb.firebaseio.com/notes.json', 
-        {
-            method: 'POST',
-            body: JSON.stringify(card)
-        });
+    getFetchData = async() => {
+        return await fetch('https://note-keeper-29879-default-rtdb.europe-west1.firebasedatabase.app/notes.json');
     }
 
-    getFetchData = async() => {
-        return await fetch('https://note-keeper-e56d5-default-rtdb.firebaseio.com/notes.json');
+    setfetchData =  async (card) => {
+        await fetch('https://note-keeper-29879-default-rtdb.europe-west1.firebasedatabase.app/notes.json', 
+            {
+                method: 'POST',
+                body: JSON.stringify(card)
+            }
+        );
     }
 
     deleteFetchData = async (id) => {
-        await fetch(`https://note-keeper-e56d5-default-rtdb.firebaseio.com/notes/${id}.json`, 
-        {
-            method: 'DELETE'
-        });
+        await fetch(`https://note-keeper-29879-default-rtdb.europe-west1.firebasedatabase.app/notes/${id}.json`, 
+            {
+                method: 'DELETE'
+            }
+        );
     }
 
     updateFetchData = async (id, obj) => {
-        await fetch(`https://note-keeper-e56d5-default-rtdb.firebaseio.com/notes/${id}.json`, 
-        {
-            method: 'PATCH',
-            body: JSON.stringify(obj)
-        });
+        await fetch(`https://note-keeper-29879-default-rtdb.europe-west1.firebasedatabase.app/notes/${id}.json`, 
+            {
+                method: 'PATCH',
+                body: JSON.stringify(obj)
+            }
+        );
     }
 }
 
